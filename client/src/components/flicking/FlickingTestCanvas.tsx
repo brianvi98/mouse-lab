08ba9b9859cd2290ad3483a018614057e76eb0da
Brian Vi <brianvi98@gmail.com>
2026-05-19 20:19:22 -0700

    feat(flicking canvas): initial canvas implementation

---
+++ client/src/components/flicking/FlickingTestCanvas.tsx
@@ -0,0 +1,127 @@
+import { useState, useRef, useEffect } from "react";
+import {
+  usePointerCapture,
+  type PointerDataPoint,
+} from "@/hooks/usePointerCapture";
+
+type FlickingTestCanvasProps = {
+  onCompletion: (data: PointerDataPoint[]) => void;
+};
+
+function FlickingTestCanvas({
+  onCompletion = () => {},
+}: FlickingTestCanvasProps) {
+  const TELEPORT_INTERVAL_MS = 3000;
+  const TEST_DURATION_MS = 15000;
+
+  const [isRecording, setIsRecording] = useState(false);
+  const [isTestComplete, setIsTestComplete] = useState(false);
+  const [timer, setTimer] = useState(TEST_DURATION_MS / 1000);
+
+  const ballRef = useRef<HTMLDivElement | null>(null);
+  const canvasRef = useRef<HTMLDivElement | null>(null);
+  const { data: pointerData } = usePointerCapture<HTMLDivElement>({
+    targetRef: canvasRef,
+    isRecording,
+  });
+
+  const moveBall = () => {
+    if (!canvasRef.current || !ballRef.current || !isRecording) return;
+
+    const canvasBounds = canvasRef.current.getBoundingClientRect();
+    const ballBounds = ballRef.current.getBoundingClientRect();
+
+    const left = Math.random() * (canvasBounds.width - ballBounds.width);
+    const top = Math.random() * (canvasBounds.height - ballBounds.height);
+
+    ballRef.current.style.left = `${left}px`;
+    ballRef.current.style.top = `${top}px`;
+  };
+
+  // teleporting logic for target pill
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      moveBall();
+    }, TELEPORT_INTERVAL_MS);
+
+    return () => clearInterval(intervalId);
+  }, [canvasRef, ballRef, isRecording]);
+
+  // timer logic
+  useEffect(() => {
+    if (!isRecording || isTestComplete) return;
+
+    const timeoutId = setTimeout(() => {
+      console.log("test complete");
+
+      setIsRecording(false);
+      setIsTestComplete(true);
+
+      onCompletion(pointerData.current);
+    }, TEST_DURATION_MS);
+
+    const timerDecrementId = setInterval(() => {
+      setTimer((t) => Math.max(0, t - 1));
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeoutId);
+      clearInterval(timerDecrementId);
+    };
+  }, [isRecording, isTestComplete, onCompletion]);
+
+  const clickStartTest = (e: React.PointerEvent<HTMLDivElement>) => {
+    console.log("starting test, recording pointer data...");
+    e.stopPropagation();
+    setIsRecording(true);
+
+    requestAnimationFrame(() => {
+      moveBall();
+    });
+  };
+
+  if (timer <= 0) {
+    return (
+      <div className="flex flex-col items-center justify-center h-full text-center">
+        <p className="text-3xl mt-32">Test Complete!</p>
+        <p className="text-m">
+          Press Escape to exit this scenario and view your results.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      ref={canvasRef}
+      className="relative w-full h-full bg-canvas flex items-center justify-center outline-2"
+    >
+      {isRecording && (
+        <h1 className="text-3xl absolute top-2 left-1/2 z-10">{timer}</h1>
+      )}
+      <div
+        ref={ballRef}
+        className="w-20 h-20 rounded-full bg-flick-orange absolute"
+      ></div>
+
+      {!isRecording && (
+        <div
+          className="flex justify-center items-center w-full h-full bg-canvas
+        absolute opacity-75"
+          onClick={clickStartTest}
+        >
+          <div className="flex flex-col items-center">
+            <p className="text-3xl">CLICK TO START</p>
+            <p className="text-m">
+              1. The target will teleport around every{" "}
+              {TELEPORT_INTERVAL_MS / 1000} seconds.
+              <br /> 2. Try to make one clean flick between the targets.
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default FlickingTestCanvas;
