9aeb3ab8975b5b370e6b90ff6c9a283be65c5fed
Brian Vi <brianvi98@gmail.com>
2026-05-20 20:10:32 -0700

    feat(flicking card): full tentative implementation of flicking test and related functionalities

--- client/src/components/flicking/FlickingTestCard.tsx
+++ client/src/components/flicking/FlickingTestCard.tsx
@@ -12,14 +12,21 @@ import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { Separator } from "../ui/separator";
 import { Button } from "../ui/button";
 import { calculateMetrics } from "@/utils/metricsCalculation";
+import type { Metric } from "@/utils/metricsCalculation";
 import type { PointerDataPoint } from "@/hooks/usePointerCapture";
 import FlickingTestCanvas from "./FlickingTestCanvas";
+import TestResultsDashboard from "../TestResultsDashboard";
+import type {
+  MetricButton,
+  AxisLabels,
+  PlotColors,
+} from "../TestResultsDashboard";
 
 function FlickingTestCard() {
   const [dialogOpen, setDialogOpen] = useState(false);
   const [sheetOpen, setSheetOpen] = useState(false);
   const [pointerData, setPointerData] = useState<PointerDataPoint[]>([]);
-  const { frames } = useMemo(() => {
+  const { frames, peakVelocity, peakAcceleration } = useMemo(() => {
     return calculateMetrics(pointerData);
   }, [pointerData]);
 
@@ -32,12 +39,30 @@ function FlickingTestCard() {
     setPointerData(data);
   };
 
+  const metricButtonsConfig: MetricButton[] = [
+    {
+      metric: "v",
+      label: "Velocity",
+      axisLabels: { xLabel: "Time (ms)", yLabel: "Velocity (px/ms)" },
+    },
+    {
+      metric: "a",
+      label: "Acceleration",
+      axisLabels: { xLabel: "Time (ms)", yLabel: "Acceleration (px/ms²)" },
+    },
+  ];
+
+  const plotColors: PlotColors = {
+    realData: "#f0a030",
+    smoothData: "#2ec4a0",
+  };
+
   return (
     <>
-      <div className="w-1/5 flex flex-col justify-center">
+      <div className="w-[320px] flex flex-col justify-center shrink-0">
         <Card
-          className="border-2 border-gray-600 hover:scale-101
-                    transition-transform duration-100 cursor-pointer"
+          className="border-2 border-gray-600 hover:scale-101 w-full
+                    transition-transform duration-100 cursor-pointer flex flex-col h-full"
           onClick={handleCardClick}
         >
           <CardHeader className="flex flex-row justify-between items-center">
@@ -46,7 +71,10 @@ function FlickingTestCard() {
               {!frames.length ? "INCOMPLETE" : "COMPLETE"}
             </div>
           </CardHeader>
-          <CardDescription className="flex flex-col justify-center align-center py-20">
+          <CardDescription
+            className="flex flex-col flex-1 justify-center 
+                      align-center py-20"
+          >
             <Button className="font-semibold text-center border w-fit mx-auto mb-2">
               START
             </Button>
@@ -55,7 +83,7 @@ function FlickingTestCard() {
             </CardContent>
           </CardDescription>
 
-          <CardFooter className="px-2 flex flex-row justify-between">
+          <CardFooter className="px-2 flex flex-row justify-between min-h-30">
             <div className="flex flex-col justify-between">
               <CardContent className="text-xs font-light text-gray-400 text-center">
                 PEAK VELOCITY
@@ -63,7 +91,7 @@ function FlickingTestCard() {
                 (px/ms)
               </CardContent>
               <CardContent className="text-center text-flick-orange tabular-nums">
-                -
+                {peakVelocity ? peakVelocity.toFixed(3) : "-"}
               </CardContent>
             </div>
             <Separator orientation="vertical" />
@@ -74,11 +102,21 @@ function FlickingTestCard() {
                 (px/ms²)
               </CardContent>
               <CardContent className="text-center text-flick-orange tabular-nums">
-                -
+                {peakAcceleration ? peakAcceleration.toFixed(3) : "-"}
               </CardContent>
             </div>
           </CardFooter>
         </Card>
+
+        <TestResultsDashboard
+          data={frames}
+          title={"Flicking"}
+          defaultMetric={"v"}
+          metricButtonsConfig={metricButtonsConfig}
+          plotColors={plotColors}
+          open={sheetOpen}
+          onOpenChange={setSheetOpen}
+        />
       </div>
 
       <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
