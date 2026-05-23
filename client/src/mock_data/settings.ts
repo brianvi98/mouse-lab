5ab28de2464364464390208b70dfffc8d2ee39e6
Brian Vi <brianvi98@gmail.com>
2026-05-23 12:07:21 -0700

    fix(mock data): turn numeric values into strings for now for easier UI compatibility until i have real backend data

--- client/src/mock_data/settings.ts
+++ client/src/mock_data/settings.ts
@@ -81,13 +81,13 @@ export const gearData = {
   ],
 
   pollingRate: [
-    { label: "125 Hz", value: 125 },
-    { label: "250 Hz", value: 250 },
-    { label: "500 Hz", value: 500 },
-    { label: "1000 Hz", value: 1000 },
-    { label: "2000 Hz", value: 2000 },
-    { label: "4000 Hz", value: 4000 },
-    { label: "8000 Hz", value: 8000 },
+    { label: "125 Hz", value: "125" },
+    { label: "250 Hz", value: "250" },
+    { label: "500 Hz", value: "500" },
+    { label: "1000 Hz", value: "1000" },
+    { label: "2000 Hz", value: "2000" },
+    { label: "4000 Hz", value: "4000" },
+    { label: "8000 Hz", value: "8000" },
   ],
 
   screenResolution: [
@@ -100,16 +100,16 @@ export const gearData = {
   ],
 
   refreshRate: [
-    { label: "60 Hz", value: 60 },
-    { label: "75 Hz", value: 75 },
-    { label: "100 Hz", value: 100 },
-    { label: "120 Hz", value: 120 },
-    { label: "144 Hz", value: 144 },
-    { label: "165 Hz", value: 165 },
-    { label: "180 Hz", value: 180 },
-    { label: "240 Hz", value: 240 },
-    { label: "360 Hz", value: 360 },
-    { label: "480 Hz", value: 480 },
-    { label: "540 Hz", value: 540 },
+    { label: "60 Hz", value: "60" },
+    { label: "75 Hz", value: "75" },
+    { label: "100 Hz", value: "100" },
+    { label: "120 Hz", value: "120" },
+    { label: "144 Hz", value: "144" },
+    { label: "165 Hz", value: "165" },
+    { label: "180 Hz", value: "180" },
+    { label: "240 Hz", value: "240" },
+    { label: "360 Hz", value: "360" },
+    { label: "480 Hz", value: "480" },
+    { label: "540 Hz", value: "540" },
   ],
 };
