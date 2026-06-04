080c086aa1684e92d20bb3ba5ba051823d07be11
Brian Vi <brianvi98@gmail.com>
2026-06-03 17:55:29 -0700

    feat(routes): added routing at the App level

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css'
-import TrackingTestCard from './components/TrackingTestCard'
+import "./App.css";
+import TestingPage from "./components/pages/TestingPage";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-  return <div className="pt-64 pl-64">
-    <TrackingTestCard />
-  </div>
+  return (
+    <div className="flex flex-col">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/testing" element={<TestingPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
