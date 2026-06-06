c43914bd575fb3f3e85a67239e3847f0b38c9aa9
Brian Vi <brianvi98@gmail.com>
2026-06-06 00:18:29 -0700

    feat(logo): extract simple logo into separate component

---
+++ client/src/components/Logo.tsx
@@ -0,0 +1,9 @@
+function Logo() {
+  return (
+    <div className="inline font-medium tracking-wide mr-4">
+      mouse<span className="text-gray-400 font-normal">lab</span>
+    </div>
+  );
+}
+
+export default Logo;
