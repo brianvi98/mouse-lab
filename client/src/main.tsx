23e07df78b3ea3b1d5e4f5038bbe67d4acbea322
Brian Vi <brianvi98@gmail.com>
2026-06-12 11:40:12 -0700

    feature(sign in): wrap App in ClerkProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/react";
+import { dark } from "@clerk/themes";
+
+const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ClerkProvider publishableKey={clerkPublishableKey} appearance={{ theme: dark }}>
+        <App />
+      </ClerkProvider>
     </BrowserRouter>
   </StrictMode>,
 );
