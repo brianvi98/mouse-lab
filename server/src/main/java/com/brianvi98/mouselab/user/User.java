30279efada6d443bc77647371265abd4fc790733
Brian Vi <brianvi98@gmail.com>
2026-07-11 18:40:35 -0700

    feat(user): mainly store clerkUserId, rely on Clerk for user data

--- server/src/main/java/com/brianvi98/mouselab/user/User.java
+++ server/src/main/java/com/brianvi98/mouselab/user/User.java
@@ -1,26 +1,27 @@
 package com.brianvi98.mouselab.user;
 
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedDate;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID uuid;
-
-    @Column(unique = true, nullable = false)
-    private String username;
+    private UUID id;
 
     @Column(name="clerk_user_id", unique = true, nullable = false)
     private String clerkUserId;
 
-    @CreatedDate
+    @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 }
