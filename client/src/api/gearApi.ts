31941815da6af0c4a9b1480f790f0c1351422402
Brian Vi <brianvi98@gmail.com>
2026-07-16 22:55:46 -0700

    feat(front api): set up gear endpoint

---
+++ client/src/api/gearApi.ts
@@ -0,0 +1,12 @@
+import { api } from "./baseApi";
+import type { GearResponse } from "./types/gear";
+
+const gearApi = api.injectEndpoints({
+  endpoints: (build) => ({
+    getGear: build.query<GearResponse, void>({
+      query: () => ({ url: "gear" }),
+    }),
+  }),
+});
+
+export const { useGetGearQuery } = gearApi;
