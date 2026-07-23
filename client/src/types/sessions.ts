5e2f7e1b99357b16265234ff82b41948a07056e3
Brian Vi <brianvi98@gmail.com>
2026-07-23 15:25:33 -0700

    feat(types): types for api

---
+++ client/src/types/sessions.ts
@@ -0,0 +1,36 @@
+import type { CalculatedMetrics } from "@/utils/metricsCalculation";
+import type { PointerSample } from "@/hooks/usePointerCapture";
+import type { GearSettings } from "./gearsettings";
+
+export type TrialType = "flicking" | "tracking";
+
+export type TrialResponse = {
+  id: string; // UUID
+  trialType: TrialType;
+  metrics: CalculatedMetrics;
+};
+
+export type TrialRequest = {
+  trialType: TrialType;
+  pointerSamples: PointerSample[];
+};
+
+export type SessionSummaryResponse = {
+  id: string; // UUID
+  createdAt: string; // Java Instant
+  mouse: string;
+  mousepad: string;
+  skates: string;
+  trialTypes: TrialType[];
+};
+
+export type SessionDetailsResponse = {
+  id: string; // UUID
+  createdAt: string; // Java Instant
+  trials: TrialResponse[];
+};
+
+export type CreateSessionRequest = {
+  settings: GearSettings;
+  trials: TrialRequest[];
+};
