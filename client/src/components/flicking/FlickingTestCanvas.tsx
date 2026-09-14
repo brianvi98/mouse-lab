99d3d7b06f66cded6a38f4905b44eb7d3a2f1caa
Brian Vi <brianvi98@gmail.com>
2026-09-14 11:13:07 -0700

    fix: unused import

--- client/src/components/flicking/FlickingTestCanvas.tsx
+++ client/src/components/flicking/FlickingTestCanvas.tsx
@@ -1,6 +1,5 @@
 import { useState, useRef, useEffect } from "react";
 import { usePointerCapture, type PointerSample } from "@/hooks/usePointerCapture";
-import { toast } from "sonner";
 
 type FlickingTestCanvasProps = {
   onCompletion: (data: PointerSample[]) => void;
